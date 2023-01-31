b0a1e41bb141f9d7350ac79ed3537f479b77f208
Estevão <contatoestevao883@hotmail.com>
2023-01-31 11:57:08 -0300

    Requisito Encomendar blusa completo

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// let nome = prompt("Qual o seu nome?");
+let nome = prompt("Qual o seu nome?");
 
 function itemSelectedModelo(pedidoClicado){
     console.log(pedidoClicado)
@@ -10,8 +10,6 @@ function itemSelectedModelo(pedidoClicado){
         selected.classList.remove('selectedModelo');
     }
     model.classList.add('selectedModelo');
-    buttonActive();
-   
 }
 
 function itemSelectedGola(pedidoClicado2){
@@ -23,7 +21,6 @@ function itemSelectedGola(pedidoClicado2){
         selected.classList.remove('selectedGola');
     }
     gola.classList.add('selectedGola');
-    buttonActive();
 
 }
 
@@ -37,21 +34,127 @@ function itemSelectedTecido(pedidoClicado3){
         selected.classList.remove('selectedTecido');
     }
     tecido.classList.add('selectedTecido');
-    buttonActive();
+}
 
+let input;
+function onChange(value){
+    if(value !== ""){
+        buttonActive()
+    }
 }
 
 function buttonActive(){
-    const input = document.querySelector('input');
     const selectedModelo = document.querySelector('.selectedModelo');
     const selectedGola = document.querySelector('.selectedGola');
     const selectedTecido = document.querySelector('.selectedTecido');
     const button = document.querySelector('.button');
     
-    if(selectedModelo !== null && selectedGola !== null && selectedTecido !== null && input.value !== null ){
-        button.disabled = false;
+    if(selectedModelo !== null && selectedGola !== null && selectedTecido !== null){
         button.style.background = '#404EED';
         button.style.color = 'white';
         button.style.cursor = 'pointer';
+        button.disabled = false;
+    }
+}
+let modelo;
+function materialModelo(materialEscolhido){
+    console.log(materialEscolhido);
+    modelo = materialEscolhido.querySelector('h3').innerText;
+    console.log(modelo);
+
+    if(modelo === 'T-shirt'){
+        modelo = 't-shirt';
+    }
+    if(modelo === 'Camiseta'){
+        modelo = 'top-tank';
+    }
+    if(modelo === 'Manga longa'){
+        modelo = 'long';
+    }
+
+}
+let gola 
+function materialGola(materialEscolhido){
+    console.log(materialEscolhido);
+    gola = materialEscolhido.querySelector('h3').innerText;
+    console.log(gola);
+
+    if(gola === 'Gola V'){
+        gola = 'v-neck';
+    }
+    if(gola === 'Gola Redonda'){
+        gola = 'round';
+    }
+    if(gola === 'Gola polo'){
+        gola = 'polo';
+    }
+
+}
+let tecido 
+function materialTecido(materialEscolhido){
+    console.log(materialEscolhido);
+    tecido = materialEscolhido.querySelector('h3').innerText;
+    console.log(tecido);
+    
+    if(tecido === 'Seda'){
+        tecido = 'silk';
+    }
+    if(tecido === 'Algodão'){
+        tecido = 'cotton';
+    }
+    if(tecido === 'Poliéster'){
+        tecido = 'polyester';
+    }
+
+}
+
+function montarBlusa(){
+    const newObject = {
+		"id": number,
+		"model": string,
+		"neck": string,
+		"material": string,
+		"image": string,
+		"owner": string,
+		"author": string
+	}
+
+    const promise = axios.get('https://mock-api.driven.com.br/api/v4/shirts-api/shirts', newObject );
+    promise.then((res) =>{
+        console.log(res);
+    })
+    promise.catch((err) => {
+        console.log(err);
+    });
+
+}
+
+montarBlusa();
+
+function mostrarBlusa(){
+    const box = document.querySelector('.box');
+    const inputValue = document.querySelector('input').value;
+
+    const newObject = {
+        "model": modelo,
+        "neck": gola,
+        "material": tecido,
+        "image": inputValue,
+        "owner": nome,
+        "author": nome
+    }
+    const promise = axios.post('https://mock-api.driven.com.br/api/v4/shirts-api/shirts', newObject)
+    promise.then((res) =>{
+        console.log(res)
+        alert('Encomenda confirmada!!!')
+    })
+    promise.catch(error)
+}
+
+function error(erro){
+    const statusCode = erro.response.status;
+    console.log(erro);
+    if ( statusCode === 422){
+        alert('Ops, não conseguimos processar sua encomenda.');
     }
 }
